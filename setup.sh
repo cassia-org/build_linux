9063ecf469e2415da062016645345ae1ddfe8397
Billy Laws <blaws05@gmail.com>
2023-07-28 13:24:52 -0700

    Update submodules

--- setup.sh
+++ setup.sh
@@ -5,19 +5,21 @@ if [ $# -lt 2 ]; then
     return
 fi
 
-export HODLL=fexcore.dll
+export HODLL=libwow64fex.dll
 export WINEPREFIX=$2
 export PATH="$1/bin:$PATH"
+WINDIR=$WINEPREFIX/drive_c/windows/
 
 if [ $# -gt 2 ]; then
 wineserver -k
-WINDIR=$WINEPREFIX/drive_c/windows/
 mkdir -p $WINDIR/system32
-cp $1/dlls/aarch64-w64-mingw32/bin/libhofex.dll  $WINDIR/system32
+cp $1/dlls/aarch64-w64-mingw32/bin/libwow64fex.dll  $WINDIR/system32
 wineboot -u
 cp $1/dlls/aarch64-w64-mingw32/bin/* $WINDIR/system32
 cp $1/dlls/i686-w64-mingw32/bin/* $WINDIR/syswow64
 fi
+
+cp $1/dlls/aarch64-w64-mingw32/bin/libwow64fex.dll  $WINDIR/system32
 
 export TU_DEBUG=mmap_32
 export WINEDLLOVERRIDES="d3d9=n,d3d11=n,d3d10core=n,dxgi=n,opengl32=n"
